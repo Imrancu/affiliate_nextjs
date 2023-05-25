d65296e0b3eabb05f0ded51b78ae91f2c547c7df
md-mobarak <md-mobarak>
2023-05-25 21:50:35 +0600

    fix issue

--- app/page.js
+++ app/page.js
@@ -8,10 +8,10 @@ import "aos/dist/aos.css"; // You can also use <link> for styles
 AOS.init();
 export default function Home() {
   return (
-    <div>
+    <>
       <Header></Header>
       <h1>hello world</h1>
       <p>I am learning from Mobark Bhai doing an affiliate project.</p>
-    </div>
+    </>
   );
 }
