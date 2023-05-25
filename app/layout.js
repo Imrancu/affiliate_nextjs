b5b1a05c706eada8523e11926859df757e8696ff
md-mobarak <md-mobarak>
2023-05-26 00:06:46 +0600

    fix issue

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Header from "./shared/Header";
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header></Header>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -3,14 +3,12 @@
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 import HeroSection from "./shared/HeroSection";
+import Header from "./shared/Header";
 // ..
 AOS.init();
 export default function Home() {
   return (
     <>
-      <Header></Header>
-      <h1>hello world</h1>
-
       <HeroSection></HeroSection>
 
       <p>I am learning from Mobark Bhai doing an affiliate project.</p>
