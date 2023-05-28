36fd54f059472ad8a66f7df1779f89e96c2202bc
Imrancu <mdimran3772@gmail.com>
2023-05-29 00:29:53 +0600

    done

--- app/page.js
+++ app/page.js
@@ -1,17 +1,16 @@
 "use client";
 
 import AOS from "aos";
-import "aos/dist/aos.css"; // You can also use <link> for styles
+import "aos/dist/aos.css"; 
 import HeroSection from "./shared/HeroSection";
-import Header from "./shared/Header";
+import Testimonial from "@/components/Testimonial";
 // ..
 AOS.init();
 export default function Home() {
   return (
     <>
       <HeroSection></HeroSection>
-
-      <p>I am learning from Mobark Bhai doing an affiliate project.</p>
+      {/* <Testimonial></Testimonial> */}
     </>
   );
 }
